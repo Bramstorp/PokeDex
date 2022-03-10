7853736a949107a7e2728810956a144abb768658
Bramstorp <m.bramstorp@stofanet.dk>
2022-03-10 22:15:47 +0100

    working on docker fix

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import vue from '@vitejs/plugin-vue'
 import ssr from 'vite-plugin-ssr/plugin'
 
 export default {
+  server: {
+    hmr: {
+      clientPort: 3001,
+    },
+  },
   plugins: [vue(), ssr()],
 }
