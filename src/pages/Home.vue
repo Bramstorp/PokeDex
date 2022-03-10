d71c145f3139fde499c0579706a9928c33fff157
Bramstorp <m.bramstorp@stofanet.dk>
2022-03-10 19:13:52 +0100

    working base

---
+++ src/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>|
+    <router-view v-slot="{ Component }">
+      <Suspense>
+        <component :is="Component" />
+      </Suspense>
+    </router-view>
+  </div>
+</template>
